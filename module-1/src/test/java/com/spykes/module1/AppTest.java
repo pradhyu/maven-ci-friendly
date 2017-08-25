4faebac5525f9f5035d73bad2a59568eaa796950
Laurentiu Marcut <laurentiu.marcut@mango.com>
2017-08-25 22:45:29 +0200

    Add Spring Boot

---
+++ module-1/src/test/java/com/spykes/module1/AppTest.java
@@ -0,0 +1,14 @@
+package com.spykes.module1;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @org.junit.Test
+    public void contextLoads() {
+    }
+}
--- module-2/src/test/java/com/spykes/module2/AppTest.java
+++ module-2/src/test/java/com/spykes/module2/AppTest.java
@@ -1,15 +1,10 @@
-package com.spykes;
+package com.spykes.module2;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
